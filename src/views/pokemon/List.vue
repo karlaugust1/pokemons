2f22992ee6fd15fbf4b894d3b5f9a7f002cafe50
Karl Harder <karl.harder@roit.com.br>
2022-10-18 19:44:37 -0300

    refactor(list): update component syntax to use composition api

--- src/views/pokemon/List.vue
+++ src/views/pokemon/List.vue
@@ -30,6 +30,7 @@
 <script>
 import PokemonCard from '@/components/PokemonCard.vue'
 import PokemonService from '@/services/PokemonService.js'
+import { ref, computed } from 'vue'
 
 export default {
   name: 'PokemonList',
@@ -37,51 +38,88 @@ export default {
   components: {
     PokemonCard
   },
-  data() {
-    return {
-      pokemons: null,
-      totalPokemons: 0
-    }
-  },
-  beforeRouteEnter(routeTo, routeFrom, next) {
-    PokemonService.getPokemons(20, parseInt(routeTo.query.page) || 0)
-      .then(response => {
+  // Composition API
+  setup(props) {
+    console.log(props.page)
+    const pokemons = ref(null)
+    const totalPokemons = ref(0)
+
+    function getPokemons(page) {
+      PokemonService.getPokemons(20, page).then(response => {
         console.log(response)
-        next(comp => {
-          comp.pokemons = response.data.results.map(pokemon => ({
-            id: pokemon.url.split('pokemon')[1].replaceAll('/', ''),
-            name: pokemon.name,
-            url: pokemon.url
-          }))
-          comp.totalPokemons = response.data.count
-          console.log(comp)
-        })
-      })
-      .catch(() => {
-        next({ name: 'NetworkError' })
-      })
-  },
-  beforeRouteUpdate(routeTo) {
-    return PokemonService.getPokemons(20, parseInt(routeTo.query.page) * 10 || 0)
-      .then(response => {
-        this.pokemons = response.data.results.map(pokemon => ({
+        pokemons.value = response.data.results.map(pokemon => ({
           id: pokemon.url.split('pokemon')[1].replaceAll('/', ''),
           name: pokemon.name,
           url: pokemon.url
         }))
-        this.totalPokemons = response.data.count
+        totalPokemons.value = response.data.count
       })
-      .catch(() => {
-        return { name: 'NetworkError' }
-      })
-  },
-  computed: {
-    hasNextPage() {
-      var totalPages = Math.ceil(this.totalPokemons / 2)
-
-      return this.page < totalPages
     }
+
+    const hasNextPage = computed(() => {
+      var totalPages = Math.ceil(totalPokemons.value / 2)
+
+      return props.page < totalPages
+    })
+
+    return { pokemons, totalPokemons, getPokemons, hasNextPage }
+  },
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    next(comp => {
+      comp.getPokemons(parseInt(routeTo.query.page) || 0)
+    }).catch(error => console.log('error', error))
+  },
+  beforeRouteUpdate(routeTo) {
+    this.getPokemons(parseInt(routeTo.query.page) * 10 || 0)
   }
+
+  // Options API
+
+  // data() {
+  //   return {
+  //     pokemons: null,
+  //     totalPokemons: 0
+  //   }
+  // },
+  // beforeRouteEnter(routeTo, routeFrom, next) {
+  //   PokemonService.getPokemons(20, parseInt(routeTo.query.page) || 0)
+  //     .then(response => {
+  //       console.log(response)
+  //       next(comp => {
+  //         comp.pokemons = response.data.results.map(pokemon => ({
+  //           id: pokemon.url.split('pokemon')[1].replaceAll('/', ''),
+  //           name: pokemon.name,
+  //           url: pokemon.url
+  //         }))
+  //         comp.totalPokemons = response.data.count
+  //         console.log(comp)
+  //       })
+  //     })
+  //     .catch(() => {
+  //       next({ name: 'NetworkError' })
+  //     })
+  // },
+  // beforeRouteUpdate(routeTo) {
+  //   return PokemonService.getPokemons(20, parseInt(routeTo.query.page) * 10 || 0)
+  //     .then(response => {
+  //       this.pokemons = response.data.results.map(pokemon => ({
+  //         id: pokemon.url.split('pokemon')[1].replaceAll('/', ''),
+  //         name: pokemon.name,
+  //         url: pokemon.url
+  //       }))
+  //       this.totalPokemons = response.data.count
+  //     })
+  //     .catch(() => {
+  //       return { name: 'NetworkError' }
+  //     })
+  // },
+  // computed: {
+  //   hasNextPage() {
+  //     var totalPages = Math.ceil(this.totalPokemons / 2)
+
+  //     return this.page < totalPages
+  //   }
+  // }
 }
 </script>
 
